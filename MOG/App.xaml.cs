3ff919f4a803a1152654eafcaf6b958153a615f4
MinhocaPirata <jgrs.seven@gmail.com>
2024-04-17 08:51:22 -0300

    inicio

---
+++ MOG/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MOG;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MOG/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MOG;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
