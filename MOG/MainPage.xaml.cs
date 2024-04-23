075a317bf27087f5ac70bd76ed18a8f92e23525e
Caio e João <jgrs.seven@gmail.com>
2024-04-23 08:53:18 -0300

    começando cs

--- MOG/MainPage.xaml.cs
+++ MOG/MainPage.xaml.cs
@@ -2,10 +2,34 @@
 
 public partial class MainPage : ContentPage
 {
+Results results;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		TestLayout();
+		PreencherTela();
+
+	}
+
+	void PreencherTela()
+	{
+		LabelTemperatura.Text=results.Temp.ToString();
+		LabelChuvax.Text=results.Rain.ToString();
+
+
+
+
+
+	}
+
+	void TestLayout()
+	{
+		results=new Results();
+		  results.Temp=44;
+		  results.Rain=25;
+
+
 	}
 
 }
